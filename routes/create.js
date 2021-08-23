c21d120f5eee05248f6e52b97b84765277a48e4c
hyunwoo045 <hyunwoo045@gmail.com>
2021-08-23 11:10:22 +0900

    feat: create content

--- routes/create.js
+++ routes/create.js
@@ -1,14 +1,29 @@
 var express = require("express");
-var url = require("url");
-// var mysql = require("mysql");
+var mysql = require("mysql");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
-  var _url = req.url;
-  var queryData = url.parse(_url, true).query;
-  console.log(queryData.id);
+router.post("/", function (req, res) {
+  let title = req.body.title;
+  let description = req.body.description;
+  let author_id = 1;
 
-  res.send("Create response");
+  let connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "111111",
+    database: "tutorial",
+  });
+  connection.connect();
+
+  connection.query(
+    `INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`,
+    [title, description, author_id],
+    (err) => {
+      if (err) throw err;
+      console.log("INSERTED SUCCESSFULLY");
+      res.send("Create Success!");
+    }
+  );
 });
 
 module.exports = router;
