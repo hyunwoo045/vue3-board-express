fff5cc52104559408147565b92d750b75261ad57
hyunwoo045 <hyunwoo045@gmail.com>
2021-08-23 12:54:47 +0900

    feat: update content

--- routes/create.js
+++ routes/create.js
@@ -3,9 +3,12 @@ var mysql = require("mysql");
 var router = express.Router();
 
 router.post("/", function (req, res) {
+  let id = req.body.id;
   let title = req.body.title;
   let description = req.body.description;
   let author_id = 1;
+
+  console.log(req.body);
 
   let connection = mysql.createConnection({
     host: "localhost",
@@ -14,16 +17,27 @@ router.post("/", function (req, res) {
     database: "tutorial",
   });
   connection.connect();
-
-  connection.query(
-    `INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`,
-    [title, description, author_id],
-    (err) => {
-      if (err) throw err;
-      console.log("INSERTED SUCCESSFULLY");
-      res.send("Create Success!");
-    }
-  );
+  if (id === "-1") {
+    connection.query(
+      `INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`,
+      [title, description, author_id],
+      (err) => {
+        if (err) throw err;
+        console.log("INSERTED SUCCESSFULLY");
+        res.send("Create Success!");
+      }
+    );
+  } else {
+    connection.query(
+      `UPDATE topic SET title=?, description=?, author_id=? WHERE id=${id}`,
+      [title, description, author_id],
+      (err) => {
+        if (err) throw err;
+        console.log("UPDATED SUCCESSFULLY");
+        res.send("Update Success!");
+      }
+    );
+  }
 });
 
 module.exports = router;
